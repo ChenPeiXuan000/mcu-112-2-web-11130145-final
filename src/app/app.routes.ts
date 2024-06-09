0eaa1f479a1cc23dbf5147f13432c1f57ee042db
Noreen <chen0901037187@gmail.com>
2024-06-10 07:38:16 +0800

    feat(router): 切換產品清單與購物車頁面實作

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ShoppingcartPageComponent } from './shoppingcart-page/shoppingcart-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  { path: 'products', component: ProductPageComponent },
+  { path: 'shoppingcart', component: ShoppingcartPageComponent },
+];
