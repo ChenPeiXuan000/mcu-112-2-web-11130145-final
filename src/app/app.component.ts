207b6bbec49411860c1da9ef9c60f42f1b7710ac
Noreen <chen0901037187@gmail.com>
2024-06-10 06:19:47 +0800

    feat(product): 變更作者資訊為文字陣列

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,28 +13,28 @@ export class AppComponent {
   products = [
     new Product({
       name: 'A 產品',
-      authors: '作者A、作者B、作者C',
+      authors: ['作者A、作者B、作者C'],
       company: '博碩文化',
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 1580,
     }),
     new Product({
       name: 'B 產品',
-      authors: '作者A、作者B、作者C',
+      authors: ['作者A、作者B、作者C'],
       company: '博碩文化',
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 1580,
     }),
     new Product({
       name: 'C 產品',
-      authors: '作者A、作者B、作者C',
+      authors: ['作者A、作者B、作者C'],
       company: '博碩文化',
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 1580,
     }),
     new Product({
       name: 'D 產品',
-      authors: '作者A、作者B、作者C',
+      authors: ['作者A、作者B、作者C'],
       company: '博碩文化',
       imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 1580,
