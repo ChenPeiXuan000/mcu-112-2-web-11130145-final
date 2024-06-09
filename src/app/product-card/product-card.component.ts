ca0200d8e4b3df703f888882c91c204310f26a15
Noreen <chen0901037187@gmail.com>
2024-06-10 02:01:08 +0800

    feat(product): 利用屬性繫結實作圖片位址繫結

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class ProductCardComponent {
   productName = 'A 產品';
-  authors = '作者甲、作者乙、作者丙';
+  authors = '作者A、作者B、作者C';
   company = '博碩文化';
+  imgUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
 }
