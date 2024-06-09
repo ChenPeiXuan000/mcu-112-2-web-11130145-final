418baa7f338da4bf26d7269fedf97ff745e8859d
Noreen <chen0901037187@gmail.com>
2024-06-10 01:50:08 +0800

    feat(product): 利用文字插植顯示產品名稱、作者與公司資訊

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -5,8 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.css'
+  styleUrl: './product-card.component.css',
 })
 export class ProductCardComponent {
-
+  productName = 'A 產品';
+  authors = '作者甲、作者乙、作者丙';
+  company = '博碩文化';
 }
