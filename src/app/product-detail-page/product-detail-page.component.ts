b0714ec7e9d254e97715696be0b93fb2b0a5c2b6
Noreen <chen0901037187@gmail.com>
2024-06-10 18:00:59 +0800

    feat(product-detail): 產品明細頁面切版實作

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,20 +1,36 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-detail-page',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.css',
 })
 export class ProductDetailPageComponent {
   productName: string;
-  product: Product | undefined;
+
+  products: Product | undefined;
 
   constructor(private route: ActivatedRoute) {
     const name = this.route.snapshot.paramMap.get('name');
     this.productName = name ? name.replace(/-/g, ' ') : '';
+  }
+
+  product = new Product({
+    name: 'A 產品',
+    authors: ['作者A、作者B、作者C'],
+    company: '博碩文化',
+    imgUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    price: 1580,
+  });
+
+  private router = inject(Router);
+
+  onBack(): void {
+    this.router.navigate(['products']);
   }
 }
